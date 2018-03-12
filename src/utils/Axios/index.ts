bf706758dfb52e733f938e9db505470e9c791f72
YaaMe <yaame@yaameMacBook.local>
2018-03-12 16:38:23 +0800

    feat(utils/Axios): error message discuss

--- src/utils/Axios/index.ts
+++ src/utils/Axios/index.ts
@@ -3,6 +3,12 @@ import {
     SERVER_HOST as HOST,
     SERVER_TIMEOUT as TIMEOUT
 } from 'config';
+import { message } from 'antd';
+
+export const error = info => {
+    message.error(info)
+    throw new Error(info)
+}
 
 export const server = axios.create({
     baseURL: HOST,
@@ -10,4 +16,14 @@ export const server = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
+    transformResponse: [(data) => {
+        let { body } = JSON.parse(data);
+        return body;
+    }]
 })
+
+export const wrapServer = opt => {
+    return server.request({
+        method: 'post',
+        ...opt
+    }).catch(() => error('请求处理失败'))}
